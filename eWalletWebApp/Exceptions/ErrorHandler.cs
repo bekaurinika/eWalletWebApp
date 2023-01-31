f1a0e0f323fae44447b5a04e27aa41cc74645769
bekaurinika <bekaurinika0@gmail.com>
2023-02-01 01:48:10 +0400

    Deleted unnecessary files

--- eWalletWebApp/Exceptions/ErrorHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eWalletWebApp.Exceptions; 
-
-public class ErrorHandler {
-    public static void HandleCustomException<T>(T exception) where T : CustomException {
-        if (exception is CustomException) {
-            // Handle custom exception
-        }
-    }
-}
